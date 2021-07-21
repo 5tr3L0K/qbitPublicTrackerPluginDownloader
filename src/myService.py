3c32a8f4f595eebec728f517ebed9b51174342c0
andresgomez <agomez@qubigo.com.ar>
2021-07-21 11:59:16 -0300

    Modificaciones varias
    Se agregan comentarios, manejo de excepciones y un sub método para la descarga de plugins,.

--- src/myService.py
+++ src/myService.py
@@ -6,74 +6,108 @@ import os
 import myClass as mc
 
 
+# proceso principal
 def main(param: mc.Param):
-    if not checkFolder(param.path):
-        print('[CREANDO] {0}'.format(param.path))
-        os.mkdir(param.path)
-    else:
-        print('[YA EXISTE] {0}'.format(param.path))
-    print('====== Descargando archivos ======')
-    download(param)
-    print('============== DONE ==============')
-    os.system('pause')
-
-
-def checkFolder(path: mc.Param.path):
-    return os.path.isdir(path)
-
-
-def checkFile(file: str):
-    return os.path.isfile(file)
-
-
-def replace():
-    rta = ''
-    while rta not in ('s', 'n'):
-        print('¿Desea remplazar los archivos en caso de que ya existan? [s/n]')
-        rta = input('=> ').lower()
-        if rta not in ('s', 'n'):
-            print('Por favor ingrese [s] o [n]...')
-    if rta == 's':
-        return True
-    else:
-        return False
-
-
-def download(param: mc.Param):
-    request = param.url
-    html = urlopen(request)
-    soup = BeautifulSoup(html, 'lxml')
-    firstTable = soup.select_one("table:nth-of-type(1)")
-    links = []
-    rep = False
-
-    for link in firstTable.findAll('a'):
-        if str(link.get('href')[-3:]) == '.py':
-            links.append(link.get('href'))
-    
-    rep = replace()
-
-    for i in links:
-        preName = str(i).split('/')
-        name = preName[-1]
-        fullName = '{0}/{1}'.format(param.path, name)
-
-        if rep:
-            if checkFile(fullName):
-                print('[REMPLAZANDO] {0}'.format(name))
-                req = re.get(str(i), allow_redirects=True)
-                with open(fullName, 'wb') as file:
-                    file.write(req.content)
-            else:
-                print('[NUEVO] {0}'.format(name))
-                req = re.get(str(i), allow_redirects=True)
-                with open(fullName, 'wb') as file:
-                    file.write(req.content)
+    proc = 'P001'
+    try:
+        if not checkFolder(param.path):
+            print('[CREANDO] {0}'.format(param.path))
+            os.mkdir(param.path)
         else:
-            if checkFile(fullName):
-                print('[YA EXISTE] {0}'.format(name))
+            print('[YA EXISTE] {0}'.format(param.path))
+        print('====== Descargando archivos ======')
+        download(param)
+        print('============== DONE ==============')
+        os.system('pause')
+    except Exception as e:
+        print('Error en: {0}'.format(proc))
+        print(e)
+
+
+# checkea si existe la carpeta de descarga
+def checkFolder(path: mc.Param.path):
+    proc = 'P002'
+    try:    
+        return os.path.isdir(path)
+    except Exception as e:
+        print('Error en: {0}'.format(proc))
+        raise e
+
+
+# checkea si el archivo existe o no
+def checkFile(file: str):
+    proc = 'P003'
+    try:
+        return os.path.isfile(file)
+    except Exception as e:
+        print('Error en: {0}'.format(proc))
+        raise e
+
+
+# consulta si se desea remplazar arhivos o no
+def replace():
+    proc = 'P004'
+    try:
+        rta = ''
+        while rta not in ('s', 'n'):
+            print('¿Desea remplazar los archivos en caso de que ya existan? [s/n]')
+            rta = input('=> ').lower()
+            if rta not in ('s', 'n'):
+                print('Por favor ingrese [s] o [n]...')
+        if rta == 's':
+            return True
+        else:
+            return False
+    except Exception as e:
+        print('Error en: {0}'.format(proc))
+        raise e
+
+
+# descarga un plugin y muestra el estado por pantalla
+def downloadSub(message, link, name, fullName: str):
+    proc = 'P006'
+    try:
+        print('[{1}] {0}'.format(name, message))
+        req = re.get(str(link), allow_redirects=True)
+        with open(fullName, 'wb') as file:
+            file.write(req.content)
+    except Exception as e:
+        print('Error en: {0}'.format(proc))
+        raise e
+
+
+# proceso principal de descarga los plugins
+def download(param: mc.Param):
+    proc = 'P005'
+    try:
+        request = param.url
+        html = urlopen(request)
+        soup = BeautifulSoup(html, 'lxml')
+        firstTable = soup.select_one("table:nth-of-type(1)")
+        links = []
+        rep = False
+
+        for link in firstTable.findAll('a'):
+            if str(link.get('href')[-3:]) == '.py':
+                links.append(link.get('href'))
+        
+        rep = replace()
+
+        for i in links:
+            preName = str(i).split('/')
+            name = preName[-1]
+            fullName = '{0}/{1}'.format(param.path, name)
+
+            if rep:
+                if checkFile(fullName):
+                    downloadSub('REMPLAZANDO', str(i), name, fullName)
+                else:
+                    downloadSub('NUEVO', str(i), name, fullName)
             else:
-                print('[NUEVO] {0}'.format(name))
-                req = re.get(str(i), allow_redirects=True)
-                with open(fullName, 'wb') as file:
-                    file.write(req.content)
+                if checkFile(fullName):
+                    print('[YA EXISTE] {0}'.format(name))
+                else:
+                    downloadSub('NUEVO', str(i), name, fullName)
+    except Exception as e:
+        print('Error en: {0}'.format(proc))
+        raise e
