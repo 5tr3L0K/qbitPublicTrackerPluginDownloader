1f2a8f1e3bab8f2e9c06ced40b5eee3493244c5f
Andrés Gómez <agomez@qubgo.com.ar>
2022-10-27 12:05:53 -0300

    Tunning installer and readme

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # qbitPublicTrackerPluginDownloader
+Download all public plugins for QbitTorrent.
 
-Use 3.10.8 Python version, not 3.11
+## Requirements
+- Chocolatey (Windows)
+https://chocolatey.org/install
+```powershell
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+```
+
+## Notes
+Use 3.10.8 Python version, not 3.11.
--- install_env.ps1
+++ install_env.ps1
@@ -1,7 +1,18 @@
-pyenv install 3.10.8
-pyenv shell 3.10.8
-python -m venv venv
-.\venv\Scripts\Activate.ps1
-python -m pip install --upgrade pip
-pip install -r requirements.txt
+Write-Host -ForegroundColor DarkGreen "INSTALANDO PYENV..."
+choco install pyenv-win -y | Out-Null
+Write-Host -ForegroundColor DarkGreen "INSTALANDO PYTHON 3.10.8..."
+pyenv install 3.10.8 | Out-Null
+Write-Host -ForegroundColor DarkGreen "ACTIVANDO PYTHON..."
+pyenv shell 3.10.8 | Out-Null
+Write-Host -ForegroundColor DarkGreen "CREANDO VENV..."
+python -m venv venv | Out-Null
+Write-Host -ForegroundColor DarkGreen "ACTIVANDO VENV..."
+.\venv\Scripts\Activate.ps1 | Out-Null
+Write-Host -ForegroundColor DarkGreen "UPGRADING PIP..."
+python -m pip install --upgrade pip | Out-Null
+Write-Host -ForegroundColor DarkGreen "INSTALANDO LIBRERIAS..."
+pip install -r requirements.txt --use-pep517 | Out-Null
+Write-Host -ForegroundColor DarkGreen "EJECUTANDO PROGRAMA...`n"
+Write-Host -ForegroundColor Cyan "###############################`n"
+$Host.UI.RawUI.ForegroundColor = "Magenta"
 python .\src\main.py
